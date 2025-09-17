fb43f550b99a7c069e61d4b70790a8a8becb5ac3
Udobros <99266268+Udobros@users.noreply.github.com>
2025-09-17 07:48:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+//  Declare your name
+let name = "Udochukwu"; 
+console.log(name);
+
+// my Favorite number
+const favoriteNumber = 7;
+console.log(favoriteNumber);
+
+//  Swapping of values
+let a = 5;
+let b = 10;
+let temp = a; // temporary variable
+a = b;
+b = temp;
+console.log("a =", a, "b =", b);
+
+//  Simple addition
+let x = 7;
+let y = 3;
+let sum = x + y;
+console.log("Sum:", sum);
+
+// Age in 10 years
+let age = 20;
+let futureAge = age + 10;
+console.log("In 10 years, I will be", futureAge);
+
+//  Temperature in Celsius
+let temperature = 25;
+console.log("Today's temperature is " + temperature + "°C");
+
+//  Full name
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName;
+console.log("Full Name:", fullName);
+
+//  Area of a rectangle
+const length = 10;
+const width = 5;
+const area = length * width;
+console.log("Area of rectangle:", area);
+
+// Days in a week
+const daysInWeek = 7;
+console.log("There are " + daysInWeek + " days in a week");
+
+
+// Bank balance
+let balance = 100;
+let deposit = 50;
+let newBalance = balance + deposit;
+console.log("New Balance:", newBalance);
